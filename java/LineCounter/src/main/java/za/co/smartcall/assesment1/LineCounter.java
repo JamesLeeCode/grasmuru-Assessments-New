fa94d4afb64235690c477a1816305cd6bb4a18d1
James Code <mazivise.jamesleeroy@gmail.com>
2022-07-18 03:45:47 +0200

    commit

--- java/LineCounter/src/main/java/za/co/smartcall/assesment1/LineCounter.java
+++ java/LineCounter/src/main/java/za/co/smartcall/assesment1/LineCounter.java
@@ -27,32 +27,32 @@ public class LineCounter {
 	 * @return return the number of actual java lines in the file
 	 * @throws IOException
 	 */
-        
+
 	public int calculateLinesInFile(URI uri) throws IOException {
 		Path path = Paths.get(uri);
 		List<String> read = Files.readAllLines(path);
-		
-		//TODO 1 the logic to calculate the number of actual lines containing code  
+
+		//TODO 1 the logic to calculate the number of actual lines containing code
 	          try {
-                     int[]iarr ={0}; //used iarr to store the total lines of code
+                     int[]iarr ={0}; //used iarr to store the total lines of code.
                     read.forEach(temp->{ //looping through all lines of code lookng for the actual code lines
                         if(temp.contains("{")||temp.contains("}")||temp.contains(";")||temp.contains("class") )
                         {
                          iarr[0]++;
                         }
                     });
-                    
+
                     return iarr[0];
                   }
                   catch (Exception e){
                   System.out.println("Something went wrong reading the file, trying another method");
                   }
-                    
+
 		//TODO 2 the method to read in the lines above crashes with very large files how would solve this?
-		// I used same logic to check the lines with code but changed the method of reading the file in case the file really big  
-           try (Stream inputStream = Files.lines(path, StandardCharsets.UTF_8)) { 
+		// I used same logic to check the lines with code but changed the method of reading the file in case the file really big
+           try (Stream inputStream = Files.lines(path, StandardCharsets.UTF_8)) {
                  BufferedReader r = new BufferedReader(new InputStreamReader((InputStream) inputStream));
-                 
+
                  String line;
                  while((line=r.readLine()) !=null){
                  int[]iarr ={0}; //used iarr to store the total lines of code
@@ -62,12 +62,12 @@ public class LineCounter {
                          iarr[0]++;
                         }
                     });
-                    
+
                     return iarr[0];
                  }
-             
-              
-               
+
+
+
            }
 		throw new IllegalArgumentException("Not implemented yet");
 	}
